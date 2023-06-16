b167d7ff01ee38b7770cd0ec60659e11bc28e7af
hjkim <hjkim_sun@naver.com>
2023-06-16 22:15:54 +0900

    코드 정리
    :

--- plugins/operators/tistory_write_post_by_chatgpt_operator.py
+++ plugins/operators/tistory_write_post_by_chatgpt_operator.py
@@ -4,7 +4,7 @@ from config.chatgpt import get_chatgpt_response
 from config.pykrx_api import get_prompt_for_chatgpt
 from config.tistory import set_tistory_post
 import pendulum
-
+from random import randrange
 class TistoryWritePostByChatgptOperator(BaseOperator):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -20,31 +20,31 @@ class TistoryWritePostByChatgptOperator(BaseOperator):
         mm = now.month
         dd = now.day
         hh = now.hour
-        ticker_name_lst, fluctuation_rate_lst, prompt_of_kospi_top_5_lst = get_prompt_for_chatgpt(now_yyyymmmdd, market='KOSPI')
+        kospi_ticker_name_lst, kospi_fluctuation_rate_lst, prompt_of_kospi_top_5_lst = get_prompt_for_chatgpt(now_yyyymmmdd, market='KOSPI')
+        kosdaq_ticker_name_lst, kosdaq_fluctuation_rate_lst, prompt_of_kosdaq_top_5_lst = get_prompt_for_chatgpt(now_yyyymmmdd, market='KOSDAQ')
+        
+        tot_ticker_name_lst = kospi_ticker_name_lst + kosdaq_ticker_name_lst
+        tot_fluctuation_rate_lst = kospi_fluctuation_rate_lst + kosdaq_fluctuation_rate_lst
+        tot_prompt = prompt_of_kospi_top_5_lst + prompt_of_kosdaq_top_5_lst
 
-        for idx, prompt in enumerate(prompt_of_kospi_top_5_lst):
-            ticker_name = ticker_name_lst[idx]
-            fluctuation_rate = fluctuation_rate_lst[idx]
+        market = 'KOSPI'
+        for idx, prompt in enumerate(tot_prompt):
+            temperature = randrange(10,100)/100     # 0.1 ~ 1 사이 
+            ticker_name = tot_ticker_name_lst[idx]
+            fluctuation_rate = tot_fluctuation_rate_lst[idx]
             fluctuation_rate = round(fluctuation_rate, 1)
             chatgpt_resp = get_chatgpt_response(api_key=chatgpt_api_key, 
                                                 prompt=prompt,
-                                                temperature=0.5)
+                                                temperature=temperature)
             chatgpt_resp = chatgpt_resp.replace('\n','<br/>')
+
+            if idx >= 5:
+                market = 'KOSDAQ'
+
             set_tistory_post(access_token=tistory_access_token,
                              blog_name='hjkim-sun',
-                             title=f'{yyyy}/{mm}/{dd} {hh}시 KOSPI 급등 {fluctuation_rate}% {ticker_name} 주목!',
+                             title=f'{yyyy}/{mm}/{dd} {hh}시 {market} 급등 {fluctuation_rate}% {ticker_name} 주목!',
                              content=chatgpt_resp,
-                             tag_lst=['KOSPI급등','급등주',ticker_name])
+                             tag_lst=[f'{market}급등','급등주',ticker_name])
             
 
-        ticker_name_lst, prompt_of_kosdaq_top_5_lst = get_prompt_for_chatgpt(now_yyyymmmdd, market='KOSDAQ')
-        for prompt in prompt_of_kosdaq_top_5_lst:
-            ticker_name = ticker_name_lst[idx]
-            chatgpt_resp = get_chatgpt_response(api_key=chatgpt_api_key, 
-                                                prompt=prompt,
-                                                temperature=0.5)
-            set_tistory_post(access_token=tistory_access_token,
-                             blog_name='hjkim-sun',
-                             title=f'{yyyy}/{mm}/{dd} {hh}시 KOSDAQ 급등주 {ticker_name} 주목!',
-                             content=chatgpt_resp,
-                             tag_lst=['KOSPI급등','급등주',ticker_name])
